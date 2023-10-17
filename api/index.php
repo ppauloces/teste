36c7cda0090732f24229841bc4db7bfae289b705
Paulo Amaral <81058578+ppauloces@users.noreply.github.com>
2023-10-17 15:45:37 -0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,9 @@
-<?php 
+<?php
+  $dsn = "mysql:host={$_ENV["DB_HOST"]};dbname={$_ENV["DB_NAME"]}";
+  $options = array(
+    PDO::MYSQL_ATTR_SSL_CA => "/etc/ssl/certs/ca-certificates.crt",
+  );
 
-phpinfo();
+  $pdo = new PDO($dsn, $_ENV["DB_USERNAME"], $_ENV["DB_PASSWORD"], $options);
+?>
+  
